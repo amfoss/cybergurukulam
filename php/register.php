2829194d8acd93a4f8622ae0bd063b5504df8d8f
risheesh <ajagratha@gmail.com>
2015-07-01 09:35:04 +0530

    Changes: For mail on suucessful registration : Added BCC

--- php/register.php
+++ php/register.php
@@ -72,6 +72,7 @@ EOSQL;
 }
 
 function insertIntoDatabase() {
+	print_r( $_POST );
 	$name = mysql_real_escape_string( $_POST['name']);
 	$email = mysql_real_escape_string( $_POST['email']);
 	$phone = mysql_real_escape_string( $_POST['phone']);
@@ -88,20 +89,19 @@ function insertIntoDatabase() {
 	$ambition= mysql_real_escape_string( $_POST['ambition'] );
 	$interest= mysql_real_escape_string( $_POST['interest'] );
 	$blog= mysql_real_escape_string( $_POST['blog'] );
+//	$timestamp = time();
 
 	$insertStatemenet = "
 	INSERT INTO `registration`(`reg_id`, `reg_name`, `reg_email`, `reg_phone`, `reg_dob`, `reg_city`, `reg_school`, `reg_address`,
-	`reg_paddress`, `reg_standard`, `reg_cse`, `reg_phy`, `reg_math`, `reg_olympiad`, `reg_ambition`, `reg_interest`, `reg_blog` )
+	`reg_paddress`, `reg_standard`, `reg_cse`, `reg_phy`, `reg_math`, `reg_olympiad`, `reg_ambition`, `reg_interest`, `reg_blog`, `reg_timestamp`)
 	VALUES ( NULL,'$name','$email','$phone','$dob','$city','$school','$address','$paddress','$standard','$cse',
-	'$phy','$math','$olympiad','$ambition','$interest', '$blog' );";
+	'$phy','$math','$olympiad','$ambition','$interest', '$blog', NULL);";
 
 	if( !mysql_query( $insertStatemenet ) ) {
-		die( mysql_error() );
-		return false;
-	} else {
+		die( mysql_error() );}
+	else { 
 	//Sending confirmation e-mail
-		sendEmail( $name, $email );
-		return true;
+		sendEmail();
 	}
 }
 
@@ -114,18 +114,21 @@ function checkIfInValidPost() {
 	return false;
 }
 
-function sendEmail( $name, $emailId ) {
-	$toAddress = 'cybergurukulam@gmail.com';
-	$message = "Hello $name \n Welcome to Cybergurukulam.\n You are successfully registered for the Entrance Test for Cybergurukulam Wintercamp 2015.\n Your e-mail ID will be the primary source communication. Make sure that you check the e-mails regularly.";
+function sendEmail() {
+	$email_to = mysql_real_escape_string( $_POST['email']);
+	$message = "Hello "+ $_POST['name'] + "\n\t\t Welcome to Cybergurukulam.\n You are successfully registered for the Entrance Test for Cybergurukulam Wintercamp 2015.\n Your e-mail ID will be the primary source communication. Make sure that you check the e-mails regularly."
 	
-	$email_to = implode( ',', $toAddress, $emailId );
 	$subject = 'Welcome to Cybergurukulam ! ';
 	$from = 'cybergurukulam@gmail.com';
 	
 	$name = 'Cybergurukulam';
 	$intro = "Message from $name\n";
 	$message = $intro.wordwrap( $message, 70, "\r\n" );
-	$headers = "From: $from" ."\r\n";
+	$headers = "From: $from" ."\r\n".
+	'Reply-To: '.$email_to."\r\n" .
+	'Bcc: cybergurukulam@gmail.com' . "\r\n" . 
+	'X-Mailer: PHP/' . phpversion();
+	
 	if ( mail( $email_to , $subject, $message, $headers ) ) {
 		header('Location: '.'/../php/success.html');
 	} else {
