e8f038420ecf7312a2d8325281032594191c11b3
risheesh <ajagratha@gmail.com>
2015-07-21 12:27:19 +0530

    trying out - connection problem

--- php/loginMethod.php
+++ php/loginMethod.php
@@ -1,86 +1,3 @@
-<!-- <!DOCTYPE html>
-<html >
-
-<head>
-    <meta charset="UTF-8">
-    <title>Secure Login: Registration Form</title>
-    <script type="text/JavaScript" src="js/sha512.js"></script>
-    <script type="text/JavaScript" src="js/forms.js"></script>
-</head>
-
-<body bgcolor="#5f9ea0" >
-
-<form action="php/loginMethod.php" method="post" name="login"></form>
-<br><br><br><br>
-
-<table width = '25%'>
-    <tr height = 50 >
-        <td> <label for="email">Email:</label></td>
-        <td><input type="text" name="email" id="email" value="" >  </td>
-    </tr>
-    <tr height = 50>
-        <td><label for="password">Password:</label></td>
-        <td><input type="password" name="password" id="password" value=""> </td>
-    </tr>
-    <tr height = 50>
-        <td>  </td>
-        <td><input type="submit" value="login" onclick="location.href='/php/loginMethod.php';" /> <input type="button" value="Register" onclick="location.href='/php/registerAdmin.php';" /></td>
-    </tr>
-</table>
-
-<!-- ?php
-
-/*
-include_once 'functions.php';
-include_once 'register.php';
-
-//include '../js/forms.js';
-
-sec_session_start();
-
-if (login_check($mysqli) == true) {
-    $logged = 'in';
-} else {
-    $logged = 'out';
-}
-
-if (isset($_POST['error'])) {
-    echo '<p class="error">Error Logging In!</p>';
-}
-$mysqli = establishConnections();
-//$mysqli = connect_db();
-if(!$mysqli){
-    header('Location: '.'error.php');
-    return;
-}
-else{echo "connected!....";}
-
-$password = formhash(this.form, this.form.password);
-
-if(login(this.form.email, $password, $mysqli)) {
-    header('Location: '.'protected_page.php');
-}
-
-if (login_check($mysqli) == true) {
-    echo '<p>Currently logged ' . $logged . ' as ' . htmlentities($_SESSION['username']) . '.</p>';
-
-    echo '<p>Do you want to change user? <a href="includes/logout.php">Log out</a>.</p>';
-} else {
-    echo '<p>Currently logged ' . $logged . '.</p>';
-    echo "<p>If you don't have a login, please <a href='registerAdmin.php'>register</a></p>";
-}
-
-mysql_close( $mysqli );
-
-?>
-</form>
-
-</body>
-</html>
--->
-
-*/
-
 <?php
 /**
  * Copyright (C) 2013 peredur.net
@@ -141,8 +58,8 @@ if (isset($_POST['error'])) {
         </tr>
     </table>
 </form>
-<p>If you don't have a login, please <a href="register.php">register</a></p>
-<p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
+<p>If you don't have a login, please <a href="registerAdmin.php">register</a></p>
+<p>If you are done, please <a href="logout.php">log out</a>.</p>
 <p>You are currently logged <?php echo $logged ?>.</p>
 </body>
 </html>
